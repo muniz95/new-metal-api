236702f132f10adda7a0ac9f59e90bc3b8f33390
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2025-08-30 02:06:54 -0300

    Refactor AlbumsController tests to use API versioning and FactoryBot for test data

--- test/controllers/albums_controller_test.rb
+++ test/controllers/albums_controller_test.rb
@@ -2,35 +2,49 @@ require 'test_helper'
 
 class AlbumsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @album = albums(:one)
+    @album = create(:album)
   end
 
   test "should get index" do
-    get albums_url, as: :json
+    get api_v1_albums_url, as: :json
     assert_response :success
   end
 
   test "should create album" do
     assert_difference('Album.count') do
-      post albums_url, params: { album: { additional_notes: @album.additional_notes, catalog_id: @album.catalog_id, description: @album.description, format: @album.format, genre: @album.genre, identifiers: @album.identifiers, label_id: @album.label_id, name: @album.name, recording_information: @album.recording_information, type: @album.type, user_id: @album.user_id } }, as: :json
+      post api_v1_albums_url, params: {
+        album: {
+          additional_notes: @album.additional_notes,
+          catalog_id: @album.catalog_id,
+          description: @album.description,
+          format: @album.format,
+          genre: @album.genre,
+          identifiers: @album.identifiers,
+          label_id: @album.label_id,
+          name: @album.name,
+          recording_information: @album.recording_information,
+          album_type: @album.album_type,
+          user_id: @album.user_id
+        }
+      }, as: :json
     end
 
     assert_response 201
   end
 
   test "should show album" do
-    get album_url(@album), as: :json
+    get api_v1_album_url(@album), as: :json
     assert_response :success
   end
 
   test "should update album" do
-    patch album_url(@album), params: { album: { additional_notes: @album.additional_notes, catalog_id: @album.catalog_id, description: @album.description, format: @album.format, genre: @album.genre, identifiers: @album.identifiers, label_id: @album.label_id, name: @album.name, recording_information: @album.recording_information, type: @album.type, user_id: @album.user_id } }, as: :json
+    patch api_v1_album_url(@album), params: { album: { additional_notes: @album.additional_notes, catalog_id: @album.catalog_id, description: @album.description, format: @album.format, genre: @album.genre, identifiers: @album.identifiers, label_id: @album.label_id, name: @album.name, recording_information: @album.recording_information, album_type: @album.album_type, user_id: @album.user_id } }, as: :json
     assert_response 200
   end
 
   test "should destroy album" do
     assert_difference('Album.count', -1) do
-      delete album_url(@album), as: :json
+      delete api_v1_album_url(@album), as: :json
     end
 
     assert_response 204
