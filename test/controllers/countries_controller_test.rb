35585875507b2090a9bd1e22e3fa7cbad241e30c
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2025-09-02 03:30:29 -0300

    Refactor CountriesControllerTest to use factory for country creation and update API URLs for consistency

--- test/controllers/countries_controller_test.rb
+++ test/controllers/countries_controller_test.rb
@@ -2,37 +2,16 @@ require 'test_helper'
 
 class CountriesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @country = countries(:one)
+    @country = create(:country)
   end
 
   test "should get index" do
-    get countries_url, as: :json
+    get api_v1_countries_url, as: :json
     assert_response :success
-  end
-
-  test "should create country" do
-    assert_difference('Country.count') do
-      post countries_url, params: { country: { name: @country.name } }, as: :json
-    end
-
-    assert_response :created
   end
 
   test "should show country" do
-    get country_url(@country), as: :json
+    get api_v1_country_url(@country), as: :json
     assert_response :success
-  end
-
-  test "should update country" do
-    patch country_url(@country), params: { country: { name: @country.name } }, as: :json
-    assert_response :ok
-  end
-
-  test "should destroy country" do
-    assert_difference('Country.count', -1) do
-      delete country_url(@country), as: :json
-    end
-
-    assert_response :no_content
   end
 end
